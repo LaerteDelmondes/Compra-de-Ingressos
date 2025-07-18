e5bdc0dd6534a189dcac8e3565769a9a782711ba
LaerteDelmondes <delmondesoliveiralaerte@gmail.com>
2025-07-18 16:18:52 -0300

    Implementando tipo Pista

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let quantidade = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    alert (tipo.value);
-    alert (quantidade.value);
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Informe uma quantidade válida!');
+        return;
+    }
+
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista!');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }    
 }
